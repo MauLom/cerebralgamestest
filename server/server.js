cac15793619d54b50d28c4bcda78923f7962ca30
Mauricio <mauricio.lombano@gmail.com>
2022-07-03 18:38:13 -0500

    Completado desarrollo sin pruebas

--- server/server.js
+++ server/server.js
@@ -2,46 +2,84 @@ const express = require('express')
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
 const WebSocket = require('ws');
+
 ///URI expuesto con pass, vulnerabilidad aca. 
-const uri ="mongodb+srv://MauLom:Hyklv5gh@cluster0.wurfvtc.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://MauLom:Hyklv5gh@cluster0.wurfvtc.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(uri);
-async function run() {
+
+async function getUserData(userId) {
   try {
     await client.connect();
     const database = client.db('test-data');
     const usersCollection = database.collection('users');
     const query = { name: 'someUserName' };
     const userData = await usersCollection.findOne(query);
-    console.log(userData.lastRuntoWheel);
+    return userData
   } finally {
     await client.close();
   }
 }
-// run().catch(console.dir);
-
-///Recibir cuando fue la ultima vez que el usuario detono la tombola
-/// Si ya paso mas de un minuto, re habilitarla (websocket)
-/// Se le otrogara un credito adicional por cada minuto en que gire la tombola
-/// podra girar otra ruleta a cambio de creditos, seran dos tipos de ruletas, una de costo de un credito, y otra de coste 2 cada ruleta tendra porcentajes de recompensa editables en la base de datos.
-
-/// El usuario debe conservar sus creditos
+async function updateUserData(newProps) {
+  try {
+    await client.connect();
+    const database = client.db('test-data');
+    const usersCollection = database.collection('users');
+    const query = { name: 'someUserName' };
+    const updateData = { $set: newProps }
+    const options = {}
+    await usersCollection.updateOne(query, updateData, options)
+  } finally {
+    await client.close();
+  }
+}
+async function getPrizesChart() {
+  try {
+    await client.connect();
+    const database = client.db('test-data');
+    const usersCollection = database.collection('settings');
+    const query = { name: 'prize-charts' };
+    const prizesData = await usersCollection.findOne(query);
+    return prizesData
+  } finally {
+    await client.close();
+  }
+}
 
 const wss = new WebSocket.Server({ port: 8000 });
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    console.log("incoming data:", data.toString('utf8'))
+
+wss.on('connection', async function connection(ws) {
+  var dataResponse = {}
+  ws.on('message', async function incoming(data) {
+    const request = JSON.parse(data.toString('utf8'))
+    switch (request.operation) {
+      case "getUserData":
+        const dataUser = await getUserData(request.userId)
+        dataResponse["dataUser"] = dataUser
+        dataResponse["operation"] = "getUserData"
+        break;
+      case "getPrizeForConection":
+        updateUserData({ lastRuntoWheel: request.lastRunToWheel, balance: request.newBalance })
+        dataResponse["operation"] = "getPrizeForConection"
+        break;
+      case "loadPrizes":
+        const prizesData = await getPrizesChart()
+        dataResponse["operation"] = "loadPrizes"
+        dataResponse["dataPrizes"] = prizesData
+        break;
+      case "rollPrizes":
+        await updateUserData({balance: request.bal})
+        const dataUserAfterRoll = await getUserData(request.userId)
+        dataResponse["dataUser"] = dataUserAfterRoll
+        dataResponse["operation"] = "rollPrizes"
+        break;
+      default:
+        dataResponse["operation"] = "Error";
+        dataResponse["message"] = "The operations was not recognized";
+        break;
+    }
+
     wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data.toString('utf8'));
-        // console.log('data', data);
-      }
+      client.send(JSON.stringify(dataResponse));
     });
   });
 });
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-// app.listen(8000, () => {
-//   console.log('Example app listening on port 8000!')
-// });
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,31 +1,88 @@
 import React, { useState, useEffect } from "react";
 
+import { Stack, Paper, Box, Button } from '@mui/material';
+
 const URL = 'ws://127.0.0.1:8000';
 
 const Main = () => {
-    const [user, setUser] = useState('Tarzan');
-    const [message, setMessage] = useState([]);
-    const [messages, setMessages] = useState([]);
+    const [userLastRunToWheel, setUserLastRunToWheel] = useState(0)
+    const [userName, setUserName] = useState('Sin datos');
+    const [balance, setBalance] = useState(0)
+    const [prizesSets, setPrizesSets] = useState([])
+    const [lastPrize, setLastPrize] = useState("")
     const [ws, setWs] = useState(new WebSocket(URL));
 
-    const submitMessage = (usr, msg) => {
-        const message = { user: usr, message: msg };
-        console.log("data sended: ", message)
-        ws.send(JSON.stringify(message));
-        setMessages([message, ...messages]);
+    const getUsersData = () => {
+        const request = { operation: "getUserData", userId: "someId" }
+        ws.send(JSON.stringify(request));
     }
+    const getPrizeForConection = () => {
+        const now = Date.now()
+        const diff = Math.abs(Date.now() - userLastRunToWheel);
+        const minutes = Math.floor((diff / 1000) / 60)
+        const request = { operation: "getPrizeForConection", lastRunToWheel: now, newBalance: (balance + 1) }
+        if (minutes >= 1) {
+            ws.send(JSON.stringify(request));
+            setUserLastRunToWheel(now)
+        }
+    }
+    const loadPrizesCharts = () => {
+        const request = { operation: "loadPrizes" }
+        ws.send(JSON.stringify(request));
+    }
+    const rollPrizes = (dataToRoll) => {
+        const arrRolls = []
+        console.log("costs", dataToRoll.cost)
+        const newBalance = balance - parseInt(dataToRoll.cost)
+        console.log("the new balance: ", newBalance)
+        if (balance > 0) {
+            const request = { operation: "rollPrizes", cost: dataToRoll.cost, bal: newBalance }
+            ws.send(JSON.stringify(request));
+            dataToRoll.details.forEach(eachDetail => {
+                const transformedProb = eachDetail.probability * 10
+                for (let i = 0; i < transformedProb; i++) {
+                    arrRolls.push(eachDetail.name)
+                }
+            })
+            const rand = Math.floor(Math.random() * 10);
+            setLastPrize(arrRolls[rand])
+        } else {
+            setLastPrize("")
+        }
 
 
-    
+    }
     useEffect(() => {
         ws.onopen = () => {
             console.log('WebSocket Connected');
         }
-
         ws.onmessage = (e) => {
-            console.log("Event on onmesagge:",e.data)
-            const message = JSON.parse(e.data);
-            setMessages([message, ...messages]);
+            const response = JSON.parse(e.data);
+            switch (response.operation) {
+                case "getUserData":
+                    setUserName(response.dataUser.name)
+                    setBalance(response.dataUser.balance)
+                    setUserLastRunToWheel(response.dataUser.lastRuntoWheel)
+                    break;
+
+                case "getPrizeForConection":
+                    const addedBalance = balance + 1
+                    setBalance(addedBalance)
+                    break;
+
+                case "loadPrizes":
+                    setPrizesSets(response.dataPrizes['prize-sets'])
+                    console.log("the charts data:", response.dataPrizes['prize-sets'])
+                    // setPrizesSets(JSON.parse(response.dataPrizes)['prize-sets'])
+                    break;
+                case "rollPrizes":
+                    setBalance(response.dataUser.balance)
+                    break;
+                default:
+                    console.error("Unidentify operation")
+                    break;
+
+            }
         }
 
         return () => {
@@ -34,45 +91,43 @@ const Main = () => {
                 setWs(new WebSocket(URL));
             }
         }
-    }, [ws.onmessage, ws.onopen, ws.onclose, messages]);
+
+    }, [ws.onmessage, ws.onopen, ws.send, ws.onclose, ws, balance]);
+
 
     return (
         <div>
-            <label htmlFor="user">
-                Name :
-                <input
-                    type="text"
-                    id="user"
-                    placeholder="User"
-                    value={user}
-                    onChange={e => setUser(e.target.value)}
-                />
-            </label>
+            <div>Hola {userName}</div>
+            <div>Tienes un balance de <b>{balance}</b> creditos</div>
+            <Button onClick={() => getUsersData()}> Cargar datos de usuario </Button>
+            <Button onClick={() => getPrizeForConection()}>Recibir premio de conexion</Button>
+            <Button onClick={() => loadPrizesCharts()}>Canjear creditos</Button>
+            {prizesSets.length > 0 ?
+                <div>
+                    <Stack sx={{ marginTop: "3%" }} direction="row" spacing={2} justifyContent="center">
+                        {prizesSets.map((eachSet, index) => (
+                            <Paper key={"key-" + eachSet.name} >
+                                Costos por giro: {eachSet.cost} credito(s)
+                                <Stack sx={{ margin: "5% 0 5% 0" }}>
+                                    {eachSet.details.map((eachDetail) => (
+                                        <Box key={"key-" + eachDetail.name}>
+                                            <Stack direction="row" spacing={5} justifyContent="space-between">
+                                                <div>{eachDetail.name}</div>
+                                                <div>{eachDetail.probability * 100}%</div>
+                                            </Stack>
+                                        </Box>
+                                    ))}
+                                </Stack>
+                                <Button onClick={() => rollPrizes(eachSet)}>Girar</Button>
+                            </Paper>
+                        ))}
+                    </Stack>
 
-            <ul>
-                {messages.reverse().map((message, index) =>
-                    <li key={index}>
-                        <b>{message.user}</b>: <em>{message.message}</em>
-                    </li>
-                )}
-            </ul>
-
-            <form
-                action=""
-                onSubmit={e => {
-                    e.preventDefault();
-                    submitMessage(user, message);
-                    setMessage([]);
-                }}
-            >
-                <input
-                    type="text"
-                    placeholder={'Type a message ...'}
-                    value={message}
-                    onChange={e => setMessage(e.target.value)}
-                />
-                <input type="submit" value={'Send'} />
-            </form>
+                </div> :
+                <></>}
+            {lastPrize !== "" ?
+                <>Ganaste {lastPrize} !</> :
+                <></>}
         </div>
     )
 }
